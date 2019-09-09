644b1edec6b0421fab6ead4e11a81e60f4e03a51
sebastian yabiku <sby2710@gmail.com>
2019-09-09 17:22:53 -0500

    it added changes

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+
 import { Header } from "./components/header/index.jsx";
 import { Main } from "./components/main/index.jsx";
 import { Footer } from "./components/footer/index.jsx";
